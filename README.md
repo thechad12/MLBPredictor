cd0cd965908ca37a78e59fcc03e0a7ba387e35f5
Kevin Pasquarella <pasquarella4292@gmail.com>
2017-07-17 21:49:50 -0400

    add initial analysis, update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MLBPredictor
+
+A project in python to analyze data from MLB games, using the MLBGame API, and to predict the outcomes of upcoming games.
---
+++ predictions/analysis.py
@@ -0,0 +1,17 @@
+from sklearn import linear_model
+import pandas as pd
+
+data = pd.read_csv('./output.csv')
+
+home_team = data['home_team']
+home_errors = data['home_errors']
+home_hits = data['home_hits']
+home_runs = data['home_runs']
+away_team = data['away_team']
+away_hits = data['away_hits']
+away_errors = data['away_errors']
+away_runs = data['away_errors']
+
+linear = linear_model.LinearRegression()
+hits = linear.fit(home_hits, home_runs)
+errors = linear.fit(home_errors, home_runs)
