76e9b1db067222f065e66a253730f953e0a69a8b
Kevin Pasquarella <pasquarella4292@gmail.com>
2017-07-20 20:25:35 -0400

    reshape data

--- predictions/analysis.py
+++ predictions/analysis.py
@@ -1,7 +1,10 @@
 from sklearn import linear_model
 import pandas as pd
+import os
 
-data = pd.read_csv('./output.csv')
+f = open(os.path.join(os.path.dirname(__file__), os.pardir, 'output.csv'))
+
+data = pd.read_csv(f)
 
 home_team = data['home_team']
 home_errors = data['home_errors']
@@ -12,10 +15,15 @@ away_hits = data['away_hits']
 away_errors = data['away_errors']
 away_runs = data['away_errors']
 
-linear = linear_model.LinearRegression()
-hits = linear.fit(home_hits, home_runs)
-errors = linear.fit(home_errors, home_runs)
 
-sc_hits = linear.score(home_hits, home_runs)
-sc_errors = linear.score(home_errors, home_runs)
+linear = linear_model.LinearRegression()
+
+# Reshape data for consistency
+hits = linear.fit(home_hits.reshape(len(home_hits),1),
+	home_runs.reshape(len(home_runs),1))
+errors = linear.fit(home_errors.reshape(len(home_errors),1),
+	home_runs.reshape(len(home_runs),1))
+
+sc_hits = linear.score(hits, home_runs)
+sc_errors = linear.score(errors, home_runs)
 
